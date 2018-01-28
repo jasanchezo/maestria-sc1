4fc503f1ca2c78ed1cf1c89f2ff3a08723928bfc
José Alejandro Sánchez Ortega <jasanchezo@gmail.com>
2018-01-27 21:12:53 -0600

    cambio pequeño desde vs code

--- servidorexp.js
+++ servidorexp.js
@@ -3,7 +3,7 @@ var expLib = require('express');
 var app = expLib();
 
 app.get('/', (req, res) => {
-    res.send('Hola Mundo');
+    res.send('Hola Mundo!');
 });
 
 app.listen(3001, () => console.log('Ejemplo app listening on port 3001'));
