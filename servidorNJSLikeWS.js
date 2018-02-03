7da25ff9ebbbd8ff7b083e9a0a47f5296dc54a1d
José Alejandro Sánchez Ortega <jasanchezo@gmail.com>
2018-02-03 00:22:19 -0600

    remosión de código basura

--- servidorNJSLikeWS.js
+++ servidorNJSLikeWS.js
@@ -1,10 +1,6 @@
 var express = require('express');
 var app = express();
 
-/* app.get('/', function (req, res) {
-  res.send('Hello World!');
-}); */
-
 app.use(express.static('.'));
 
 app.listen(3000, function () {
