bd83df16db34923e68291ea9a238e25918963a90
José Alejandro Sánchez Ortega <jasanchezo@gmail.com>
2018-02-10 16:04:30 -0600

    cambios para estructurar en plantilla

--- servidorexp.js
+++ servidorexp.js
@@ -1,9 +1,27 @@
-var expLib = require('express');
+// DEFINICIÓN DE LIBRERIAS DE NODE JS
+const expressLib = require('express');
+const dirLib = require('path');
+const plantilla = require('ejs');
 
-var app = expLib();
+// INSTANCIAMOS EXPRESS
+var app = expressLib();
+
+app.use(expressLib.static('public'));
+app.use(expressLib.static('views'));
+app.set("view engine", "ejs");
 
 app.get('/', (req, res) => {
-    res.send('Hola Mundo!');
+    // res.send('Hola Mundo!');
+    // res.sendFile(dirLib.join(__dirname+'/index.html'));
+    // res.sendFile(dirLib.join(__dirname+'../views/index.ejs'));
+
+    res.render("page");
 });
 
-app.listen(3001, () => console.log('Ejemplo app listening on port 3001'));
+app.get('/contact', (req, res) => {
+    res.render("contact");
+});
+
+
+// POSICIONAMOS 
+app.listen(3000, () => console.log('Ejemplo app listening on port 3000'));
