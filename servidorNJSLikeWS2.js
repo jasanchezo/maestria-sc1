7128b4b68456f9efb38fb93d841e15c0dcac97ba
José Alejandro Sánchez Ortega <jasanchezo@gmail.com>
2018-02-10 14:07:44 -0600

    ok

--- servidorNJSLikeWS2.js
+++ servidorNJSLikeWS2.js
@@ -1,9 +1,13 @@
 // REFERENCIA: http://expressjs.com/es/starter/hello-world.html 
 
 var express = require('express');
-var app = express();
+var express = require('path');
+
+var app = expressLib();
 
 app.use(express.static('./public'));
+
+
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
