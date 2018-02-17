9e33e70a5a00018ee2fc4ceaa66b9c32f2a2634e
José Alejandro Sánchez Ortega <jasanchezo@gmail.com>
2018-02-16 21:30:29 -0600

    creación de secciones con EJS y generación de rutas para cada sección

--- servidorexp.js
+++ servidorexp.js
@@ -6,22 +6,57 @@ const plantilla = require('ejs');
 // INSTANCIAMOS EXPRESS
 var app = expressLib();
 
+// PREPARAMOS EL ENTORNO GENERAL
 app.use(expressLib.static('public'));
 app.use(expressLib.static('views'));
 app.set("view engine", "ejs");
 
+// RUTEO PARA PAGE
 app.get('/', (req, res) => {
     // res.send('Hola Mundo!');
     // res.sendFile(dirLib.join(__dirname+'/index.html'));
     // res.sendFile(dirLib.join(__dirname+'../views/index.ejs'));
 
-    res.render("page");
+    console.log(req.originalUrl);
+
+    res.render("index");
 });
 
+// RUTEO PARA SERVICE
+app.get('/service', (req, res) => {
+    console.log(req.originalUrl);
+    res.render("service");
+});
+
+// RUTEO PARA TEAM
+app.get('/team', (req, res) => {
+    console.log(req.originalUrl);
+    res.render("team");
+});
+
+// RUTEO PARA SKILLS
+app.get('/skills', (req, res) => {
+    console.log(req.originalUrl);
+    res.render("skills");
+});
+
+// RUTEO PARA PORTFOLIO
+app.get('/portfolio', (req, res) => {
+    console.log(req.originalUrl);
+    res.render("portfolio");
+});
+
+// RUTEO PARA PRICE
+app.get('/price', (req, res) => {
+    console.log(req.originalUrl);
+    res.render("price");
+});
+
+// RUTEO PARA CONTACT
 app.get('/contact', (req, res) => {
+    console.log(req.originalUrl);
     res.render("contact");
 });
 
-
-// POSICIONAMOS 
+// POSICIONAMOS EL SERVICIO EN EL PUERTO 3000/TCP
 app.listen(3000, () => console.log('Ejemplo app listening on port 3000'));
