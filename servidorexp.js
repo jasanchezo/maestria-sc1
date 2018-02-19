c2668e54d263c308c4738bc3fc8c3583cd445747
José Alejandro Sánchez Ortega <jasanchezo@gmail.com>
2018-02-18 22:30:25 -0600

    cmabios generados en la clase del sábado 17 de febereo

--- servidorexp.js
+++ servidorexp.js
@@ -2,16 +2,23 @@
 const expressLib = require('express');
 const dirLib = require('path');
 const plantilla = require('ejs');
+const parseador = require('body-parser');
 
 // INSTANCIAMOS EXPRESS
 var app = expressLib();
+
+// PARÁMETRO DE CONFIGURACIÓN DE BODY-PASER
+var urlencodedParser = parseador.urlencoded({ extended: false });
+
+// HABILITAMOS BODY-PARSER COMO PARSER JSON
+app.use(parseador.json());
 
 // PREPARAMOS EL ENTORNO GENERAL
 app.use(expressLib.static('public'));
 app.use(expressLib.static('views'));
 app.set("view engine", "ejs");
 
-// RUTEO PARA PAGE
+// RUTEO PARA INDEX
 app.get('/', (req, res) => {
     // res.send('Hola Mundo!');
     // res.sendFile(dirLib.join(__dirname+'/index.html'));
@@ -58,5 +65,22 @@ app.get('/contact', (req, res) => {
     res.render("contact");
 });
 
+// POST /login gets urlencoded bodies
+app.post('/contact', urlencodedParser, function (req, res) {
+    if (!req.body) return res.sendStatus(400);
+    console.log(req.body.name + " " + req.body.email + " " + req.body.phone + " " + req.body.website + " " + req.body.message);
+    // MOSTRAR SOLO EL DATO CAPTURADO
+    res.send('welcome, ' + req.body.name + " " + req.body.email + " " + req.body.phone + " " + req.body.website + " " + req.body.message);
+    // res.render("contact");
+  });
+
 // POSICIONAMOS EL SERVICIO EN EL PUERTO 3000/TCP
 app.listen(3000, () => console.log('Ejemplo app listening on port 3000'));
+
+/*
+FORMA DE RECIBIR CUALQUIER DATO EN EL REQUEST
+app.get('*', (req, res) => {
+    console.log(req.originalUrl);
+    res.render("contact");
+});
+*/
